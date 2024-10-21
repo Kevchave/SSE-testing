9bc21b5fd61231d3737ff32f90894d06f08ae69f
Kevin Chave <kevin.chave24@imperial.ac.uk>
2024-10-21 15:41:04 +0100

    Testing test yml

--- src/app_test.py
+++ src/app_test.py
@@ -5,5 +5,6 @@ def test_knows_about_dinosaurs():
     dino = "Dinosaurs ruled the Earth 200 million years ago"
     assert process_query("dinosaurs") == dino
 
+
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
