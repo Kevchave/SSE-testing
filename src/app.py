e47bf12abf3fe7283c4139646f987fff36463055
Kevin Chave <kevin.chave24@imperial.ac.uk>
2024-10-21 15:26:04 +0100

    Testing test yml

--- src/app.py
+++ src/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
-
 
 @app.route("/submit", methods=["POST"])
 def submit():
@@ -13,9 +12,9 @@ def submit():
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
 
-
 def process_query(query):
-	if (query == "dinosaurs"):
-		return "Dinosaurs ruled the Earth 200 million years ago"
-	else: 
-		return "Unknown"
+    if query == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    else: 
+        return "Unknown"
+
