1ec9a3245cf6a7bb257db65fd38195c00ba91703
Kevin Chave <kevin.chave24@imperial.ac.uk>
2024-10-21 14:43:10 +0100

    fixed python file

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
